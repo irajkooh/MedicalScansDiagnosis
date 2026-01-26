0c62146489041d2c5c4594fd02129575908c141f
irajkooh <irajkoohi@gmail.com>
2026-01-26 17:44:13 -0600

    Fix Gradio 6.0 deprecation warning

--- app.py
+++ app.py
@@ -164,8 +164,7 @@ custom_css = """
 """
 
 # Create Gradio interface with Blocks for custom copy button
-#with gr.Blocks(title="🏥 MedGemma 1.5: Medical Image Analysis") as demo:
-with gr.Blocks(title="🏥 MedGemma 1.5: Medical Image Analysis", css=custom_css) as demo:
+with gr.Blocks(title="🏥 MedGemma 1.5: Medical Image Analysis") as demo:
 
     gr.Markdown("# 🏥 MedGemma 1.5: Medical Image Analysis")
     gr.Markdown("""
@@ -287,7 +286,8 @@ if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0",
         server_port=7860,
-        inbrowser=True
+        inbrowser=True,
+        css=custom_css
     )
 
 
